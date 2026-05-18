7fc9e3f5d07a0b49a972d2b91631611a51093153
pritt18 <pritamgangurde18@gmail.com>
2026-05-18 20:14:39 +0530

    Updated API URL config

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,17 +1,22 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_URL,
 });
 
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+API.interceptors.request.use(
+  (config) => {
+    const token =
+      localStorage.getItem("token");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    if (token) {
+      config.headers.Authorization =
+        `Bearer ${token}`;
+    }
+
+    return config;
   }
-
-  return config;
-});
+);
 
 export default API;
